28907fa356052f3958fcf720a84149891f0400f6
practicante <practicante@MDYLN.COM>
2018-09-07 12:42:05 -0500

    testing server/comentar modelo knn

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(RMySQL)
 library(xlsx)
 library(caret)
 source("crcoan.R")
-mknn=creaModelknn()
+# mknn=creaModelknn()
 
 shinyServer(function(input, output, session) {
   output$resp=renderText({
