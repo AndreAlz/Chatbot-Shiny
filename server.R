3dc2ee2d9af022dbc4530fe9d1d3c830c8009eca
practicante <practicante@MDYLN.COM>
2018-09-07 12:38:49 -0500

    testing server

--- server.R
+++ server.R
@@ -7,42 +7,45 @@ source("crcoan.R")
 mknn=creaModelknn()
 
 shinyServer(function(input, output, session) {
-
-  mensaje=eventReactive(input$enviar,input$texto)
-  pre=reactiveVal(0)
-  interact=0
-  observeEvent(input$enviar,{
-    if (interact==0) {
-      data=partirmensaje(mensaje())
-      newvalue=predict(mknn,data[,2:5])
-      pre(newvalue)
-      updateTextInput(session,inputId = "texto", value = "")
-    }
+  output$resp=renderText({
+    "prueba de servidor"
   })
-  
-  observeEvent(pre(),{
-    valo=as.numeric(pre())
-    if(valo==1){
-      output$resp=renderText("ok pero necesito tu numero de ticket")
-      interact<<-1
-    }
-    if(valo==2){
-      output$resp=renderText("desde que fecha")
-      interact<<-1
-    }
-  })
-  observeEvent(input$enviar,{
-    updateTextInput(session,inputId = "texto", value = "")
-    if(interact==1){
-      valo=as.numeric(pre())
-      if(valo==1){
-        output$resp=renderText("tipo 2.1")
-      }
-      if(valo==2){
-        output$resp=renderText("tipo 2.2")
-      }
-      interact<<-0
-      pre(0)
-    }
-  })
+# 
+#   mensaje=eventReactive(input$enviar,input$texto)
+#   pre=reactiveVal(0)
+#   interact=0
+#   observeEvent(input$enviar,{
+#     if (interact==0) {
+#       data=partirmensaje(mensaje())
+#       newvalue=predict(mknn,data[,2:5])
+#       pre(newvalue)
+#       updateTextInput(session,inputId = "texto", value = "")
+#     }
+#   })
+# 
+#   observeEvent(pre(),{
+#     valo=as.numeric(pre())
+#     if(valo==1){
+#       output$resp=renderText("ok pero necesito tu numero de ticket")
+#       interact<<-1
+#     }
+#     if(valo==2){
+#       output$resp=renderText("desde que fecha")
+#       interact<<-1
+#     }
+#   })
+#   observeEvent(input$enviar,{
+#     updateTextInput(session,inputId = "texto", value = "")
+#     if(interact==1){
+#       valo=as.numeric(pre())
+#       if(valo==1){
+#         output$resp=renderText("tipo 2.1")
+#       }
+#       if(valo==2){
+#         output$resp=renderText("tipo 2.2")
+#       }
+#       interact<<-0
+#       pre(0)
+#     }
+#   })
 })
